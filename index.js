f40b5c33aefb6f7e2a1f5a09936ad6d55e0a91ea
Michael Elfassy <mike.elfassy@gmail.com>
2022-11-29 22:46:47 -0500

    commit tree 5f873e7642dba5c754585e8037069e9b90e0558e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, adjective) {
+    let allCards = []
+    for(let name of names){
+        allCards.push(`Thank you, ${name}, for the wonderful ${adjective} gift!`)
+    }
+    console.log(allCards)
+    return allCards
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
+countDown(4)
